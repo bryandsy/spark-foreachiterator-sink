bf2006848817ecf575946abb289d382a1ae14754
Nguyen Duc Hoa <hoameomu@gmail.com>
2017-10-30 10:52:34 +0800

    Fix extra parameter error

--- core/src/main/scala/com/idyllic/spark/streaming/AbstractWriterTask.scala
+++ core/src/main/scala/com/idyllic/spark/streaming/AbstractWriterTask.scala
@@ -3,7 +3,7 @@ package com.saikocat.spark.streaming
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.catalyst.encoders.ExpressionEncoder
 
-abstract class AbstractWriteTask[T](
+abstract class AbstractWriteTask[T] {
   def parameters: Map[String, String]
   def expressionEncoder: ExpressionEncoder[T]
 
@@ -15,7 +15,7 @@ abstract class AbstractWriteTask[T](
     while (iterator.hasNext) {
       val currentRow: InternalRow = iterator.next()
       val record: T = expressionEncoder.fromRow(currentRow)
-      writeRecordFunc(pipeline, record)
+      writeRecordFunc(record)
     }
   }
 
